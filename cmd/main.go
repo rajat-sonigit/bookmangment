21571773aa76139d92c27dc43c45ab5d33b3daa8
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-03-25 13:55:59 +0530

    Add files via upload

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rajat-sonigit/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStore(r)
+	fmt.Printf("Port started")
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+}
